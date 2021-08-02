d5890d42fc3205dcd5a5f1e4a823bf7247e0f9ba
Neyt <chanchristianarana@gmail.com>
2021-08-02 10:41:48 +0800

    m13
    Fix Approval conde implementing funcitons and Some alert notification changes when approval event

--- php/cnfg.php
+++ php/cnfg.php
@@ -4,8 +4,8 @@ $host = '127.0.0.1';
 $db   = 'telford_db';
 $user = 'root';
 // $user = 'neyt';
-// $pass = 'gieRHAAA9iSi3ULZ';
-$pass = 'dkoalam3h';
+$pass = 'gieRHAAA9iSi3ULZ';
+// $pass = 'dkoalam3h';
 $charset = 'utf8mb4';
 
 $options = [
